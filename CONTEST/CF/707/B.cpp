c9a684cf2c92077c8f8c9739bd3cda9db1e1f01f
ksb <iec2018043@iiita.ac.in>
2021-04-25 00:29:48 +0530

    707

---
+++ CONTEST/CF/707/B.cpp
@@ -0,0 +1,118 @@
+//**sn0wrus**//
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cout.tie(NULL);                   \
+    cin.tie(NULL);
+#define write(a)          \
+    for (auto x : a)      \
+    {                     \
+        cout << x << " "; \
+    }                     \
+    cout << endl;
+#define read(a)       \
+    for (auto &x : a) \
+    {                 \
+        cin >> x;     \
+    }
+#define IN cin >>
+#define OUT cout <<
+#define endl "\n"
+#define all(a) (a).begin(), (a).end()
+#define allr(a) (a).rbegin(), (a).rend()
+#define pb push_back
+
+#define fi first
+#define se second
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x ;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'<<endl;}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}"<<endl;}
+
+
+using ll = long long int;
+using ld = long double;
+using pll = pair<ll, ll>;   
+using pii = pair<int, int>;
+using plll  = pair<ll,pll>;
+using vll = vector<ll>;
+using vi = vector<int>;
+
+const ll MOD = (ll)(1e9) + 7LL;
+const ll MM = 998244353LL;
+const ll INF  = INT_MAX;
+
+const int dir8[8][2]={{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+const int dir4[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
+
+const ll MAXN = 200005;
+
+/*
+for(int i=0;i<n-1;i++)
+{
+    int a,b;
+    cin>>a>>b;
+    a--,b--;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+}
+*/
+
+void solve()
+{
+	ll n;
+    cin>>n;
+    vector<ll>arr(n);
+    for(int i=0;i<n;i++)cin>>arr[i];
+    vector<ll>ans(n,0);
+	for(int i=0;i<n;i++)
+	{
+		if(arr[i]>0)
+			ans[max(0LL, i- arr[i]+1)] = min(arr[i],i+1LL);
+	}
+	ll curr = 0;
+	// __print(ans);
+	for(int i=0;i<n;i++)
+	{
+		if((curr == 0) && (ans[i]==0))
+		{
+			cout<<0<<" ";
+		}
+		else{
+			cout<<1<<" ";
+			curr = max(curr, ans[i]);
+			curr--;
+		}
+	}
+	cout<<endl;
+	return;
+}
+
+int main()
+{
+    fast;
+    ll tc = 1;
+    IN tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ CONTEST/CF/707/C.cpp
@@ -0,0 +1,116 @@
+//**sn0wrus**//
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cout.tie(NULL);                   \
+    cin.tie(NULL);
+#define write(a)          \
+    for (auto x : a)      \
+    {                     \
+        cout << x << " "; \
+    }                     \
+    cout << endl;
+#define read(a)       \
+    for (auto &x : a) \
+    {                 \
+        cin >> x;     \
+    }
+#define IN cin >>
+#define OUT cout <<
+#define endl "\n"
+#define all(a) (a).begin(), (a).end()
+#define allr(a) (a).rbegin(), (a).rend()
+#define pb push_back
+
+#define fi first
+#define se second
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x ;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}'<<endl;}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}"<<endl;}
+
+
+using ll = long long int;
+using ld = long double;
+using pll = pair<ll, ll>;   
+using pii = pair<int, int>;
+using plll  = pair<ll,pll>;
+using vll = vector<ll>;
+using vi = vector<int>;
+
+const ll MOD = (ll)(1e9) + 7LL;
+const ll MM = 998244353LL;
+const ll INF  = INT_MAX;
+
+const int dir8[8][2]={{1,0},{0,1},{-1,0},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+const int dir4[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
+
+const ll MAXN = 1000000;
+
+/*
+for(int i=0;i<n-1;i++)
+{
+    int a,b;
+    cin>>a>>b;
+    a--,b--;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+}
+*/
+
+void solve()
+{
+	ll n;
+    cin>>n;
+    vector<ll>arr(n);
+    for(int i=0;i<n;i++)cin>>arr[i];
+    vector<vector<pll>>sum(MAXN*5);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<i;j++)
+		{
+			ll curr = arr[i]+arr[j];
+			for(auto x:sum[curr])
+			{
+				if((i!=x.first)&&(i!=x.second)&&(j!=x.first)&&(j!=x.second))
+				{
+					cout<<"YES"<<endl;
+					cout<<i+1<<" "<<j+1<<" "<<x.first+1<<" "<<x.second+1<<endl;
+					return;
+				}
+			}
+			sum[curr].push_back({i,j});
+		}
+	}
+	cout<<"NO"<<endl;
+	return;
+}
+
+int main()
+{
+    fast;
+    ll tc = 1;
+    //IN tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
